c42a992434385350fd338b169e3de6bf1fc68919
toxic-johann <toxic.johann@gmail.com>
2018-02-08 09:45:47 +0800

    [update] add multiple async chunk test case

---
+++ tests/fixtures/file-a.js
@@ -0,0 +1,4 @@
+console.log('=-=');
+import('./module-a.js');
+import('./home.js');
+import('./vendor.js');
--- tests/index.test.js
+++ tests/index.test.js
@@ -4,6 +4,7 @@ const WebpackCdnUploadPlugin = require('../index');
 const path = require('path');
 const OUTPUT_DIR = path.join(__dirname, 'dist');
 const CDN_PREFIX = 'http://cdn.toxicjohann.com/';
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 describe('base behavior test', () => {
   beforeEach(() => {
@@ -70,6 +71,38 @@ describe('base behavior test', () => {
       const scripts = document.head.getElementsByTagName('script');
       expect(scripts.length).toBe(1);
       expect(scripts[0].src).toBe(CDN_PREFIX + '0.js');
+      done();
+    });
+    compiler.outputFileSystem = new MemoryFileSystem();
+  });
+
+  test('replace url for multiple chunk', done => {
+    const compiler = webpack({
+      entry: {
+        file: path.join(__dirname, 'fixtures', 'file-a.js'),
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js',
+      },
+      plugins: [
+        new WebpackCdnUploadPlugin({
+          upload(content, name) {
+            return CDN_PREFIX + name;
+          },
+          replaceAsyncChunkName: true,
+        }),
+      ],
+    }, function(error, result) {
+      expect(error).toBeFalsy();
+      expect(result.compilation.errors.length).toBe(0);
+      const js = result.compilation.assets['file.js'].source();
+      eval(js);
+      const scripts = document.head.getElementsByTagName('script');
+      expect(scripts.length).toBe(3);
+      Array.from(scripts).forEach(script => {
+        expect(script.src.indexOf(CDN_PREFIX)).toBe(0);
+      });
       done();
     });
     compiler.outputFileSystem = new MemoryFileSystem();
