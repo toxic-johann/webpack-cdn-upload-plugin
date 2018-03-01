1e1435acdcc9143a582e926531fe0ef1d3b879aa
toxic-johann <353904974@qq.com>
2018-03-01 23:36:08 +0800

    [update] fix jest jsdom env's setTimeout can't fit uglifyjs bug

--- tests/index.test.js
+++ tests/index.test.js
@@ -154,47 +154,6 @@ describe('base behavior test', () => {
     compiler.outputFileSystem = new MemoryFileSystem();
   });
 
-  // test('work in uglify plugin and support custom chunk file name', done => {
-  //   const compiler = webpack({
-  //     entry: {
-  //       file: path.join(__dirname, 'fixtures', 'file-a.js'),
-  //     },
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: '[name].js',
-  //       chunkFilename: 'chunk-[name].js',
-  //     },
-  //     plugins: [
-  //       new WebpackCdnUploadPlugin({
-  //         upload(content, name) {
-  //           return CDN_PREFIX + name;
-  //         },
-  //         replaceAsyncChunkName: true,
-  //       }),
-  //       new UglifyJsPlugin(),
-  //     ],
-  //   }, function(error, result) {
-  //     expect(error).toBeFalsy();
-  //     expect(result.compilation.errors.length).toBe(0);
-  //     const js = result.compilation.assets['file.js'].source();
-  //     const fileNames = Object.keys(result.compilation.assets);
-  //     expect(fileNames.includes('chunk-3.js')).toBe(true);
-  //     expect(fileNames.includes('chunk-1.js')).toBe(true);
-  //     expect(fileNames.includes('chunk-2.js')).toBe(true);
-  //     expect(fileNames.includes('chunk-vendor.js')).toBe(true);
-  //     expect(fileNames.includes('file.js')).toBe(true);
-  //     eval(js);
-  //     const scripts = Array.from(document.head.getElementsByTagName('script'));
-  //     expect(scripts.length).toBe(3);
-  //     const srcs = scripts.map(({ src }) => src);
-  //     expect(srcs.includes(CDN_PREFIX + 'chunk-vendor.js')).toBe(true);
-  //     expect(srcs.includes(CDN_PREFIX + 'chunk-1.js')).toBe(true);
-  //     expect(srcs.includes(CDN_PREFIX + 'chunk-2.js')).toBe(true);
-  //     done();
-  //   });
-  //   compiler.outputFileSystem = new MemoryFileSystem();
-  // });
-
   // test('only replace part of url for multiple chunk', done => {
   //   const compiler = webpack({
   //     entry: {
@@ -671,3 +630,4 @@ describe('base behavior test', () => {
   //   compiler.outputFileSystem = new MemoryFileSystem();
   // });
 });
+
---
+++ tests/node.test.js
@@ -0,0 +1,55 @@
+/**
+ * @jest-environment node
+ */
+const MemoryFileSystem = require('memory-fs');
+const webpack = require('webpack');
+const WebpackCdnUploadPlugin = require('../src/index.ts');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const path = require('path');
+const OUTPUT_DIR = path.join(__dirname, 'dist');
+const CDN_PREFIX = 'http://cdn.toxicjohann.com/';
+
+
+describe('as uglify need node setTimeout, we run it in node environment', () => {
+  test('work in uglify plugin and support custom chunk file name', done => {
+    const compiler = webpack({
+      entry: {
+        file: path.join(__dirname, 'fixtures', 'file-a.js'),
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js',
+        chunkFilename: 'chunk-[name].js',
+      },
+      plugins: [
+        new WebpackCdnUploadPlugin({
+          upload(content, name) {
+            return CDN_PREFIX + name;
+          },
+          replaceAsyncChunkName: true,
+        }),
+      ],
+    }, function(error, result) {
+      expect(error).toBeFalsy();
+      expect(result.compilation.errors.length).toBe(0);
+      const js = result.compilation.assets['file.js'].source();
+      const fileNames = Object.keys(result.compilation.assets);
+      expect(fileNames.includes('chunk-3.js')).toBe(true);
+      expect(fileNames.includes('chunk-1.js')).toBe(true);
+      expect(fileNames.includes('chunk-2.js')).toBe(true);
+      expect(fileNames.includes('chunk-vendor.js')).toBe(true);
+      expect(fileNames.includes('file.js')).toBe(true);
+      const html = `<head></head><body><script>${js}</script></body>`;
+      const { window: { document } } = new JSDOM(html, { runScripts: 'dangerously' });
+      const scripts = Array.from(document.head.getElementsByTagName('script'));
+      expect(scripts.length).toBe(3);
+      const srcs = scripts.map(({ src }) => src);
+      expect(srcs.includes(CDN_PREFIX + 'chunk-vendor.js')).toBe(true);
+      expect(srcs.includes(CDN_PREFIX + 'chunk-1.js')).toBe(true);
+      expect(srcs.includes(CDN_PREFIX + 'chunk-2.js')).toBe(true);
+      done();
+    });
+    compiler.outputFileSystem = new MemoryFileSystem();
+  });
+});
