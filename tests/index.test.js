3f923c2ce5f3ed176ca906a71a092d9277485c7d
toxic-johann <toxic.johann@gmail.com>
2018-03-02 13:43:09 +0800

    [update] pass two more tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -195,87 +195,89 @@ describe('base behavior test', () => {
     compiler.outputFileSystem = new MemoryFileSystem();
   });
 
-  // test('only replace part of url for multiple chunk and support publich path', done => {
-  //   const compiler = webpack({
-  //     entry: {
-  //       file: path.join(__dirname, 'fixtures', 'file-a.js'),
-  //     },
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: '[name].js',
-  //       publicPath: '/public/',
-  //     },
-  //     plugins: [
-  //       new WebpackCdnUploadPlugin({
-  //         upload(content, name) {
-  //           if (name !== '0.js') return;
-  //           return CDN_PREFIX + name;
-  //         },
-  //         replaceAsyncChunkName: true,
-  //       }),
-  //     ],
-  //   }, function(error, result) {
-  //     expect(error).toBeFalsy();
-  //     expect(result.compilation.errors.length).toBe(0);
-  //     const js = result.compilation.assets['file.js'].source();
-  //     const fileNames = Object.keys(result.compilation.assets);
-  //     expect(fileNames.includes('0.js')).toBe(true);
-  //     expect(fileNames.includes('1.js')).toBe(true);
-  //     expect(fileNames.includes('2.js')).toBe(true);
-  //     expect(fileNames.includes('3.js')).toBe(true);
-  //     expect(fileNames.includes('file.js')).toBe(true);
-  //     // fs.writeFileSync('./what.js', js);
-  //     eval(js);
-  //     const scripts = Array.from(document.head.getElementsByTagName('script'));
-  //     expect(scripts.length).toBe(3);
-  //     const srcs = scripts.map(({ src }) => src);
-  //     expect(srcs.includes(CDN_PREFIX + '0.js')).toBe(true);
-  //     expect(srcs.includes('/public/1.js')).toBe(true);
-  //     expect(srcs.includes('/public/2.js')).toBe(true);
-  //     done();
-  //   });
-  //   compiler.outputFileSystem = new MemoryFileSystem();
-  // });
+  test('only replace part of url for multiple chunk and support publich path', done => {
+    const compiler = webpack({
+      mode: 'development',
+      entry: {
+        file: path.join(__dirname, 'fixtures', 'file-a.js'),
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js',
+        publicPath: '/public/',
+      },
+      plugins: [
+        new WebpackCdnUploadPlugin({
+          upload(content, name) {
+            if (name !== '0.js') return;
+            return CDN_PREFIX + name;
+          },
+          replaceAsyncChunkName: true,
+        }),
+      ],
+    }, function(error, result) {
+      expect(error).toBeFalsy();
+      expect(result.compilation.errors.length).toBe(0);
+      const js = result.compilation.assets['file.js'].source();
+      const fileNames = Object.keys(result.compilation.assets);
+      expect(fileNames.includes('0.js')).toBe(true);
+      expect(fileNames.includes('1.js')).toBe(true);
+      expect(fileNames.includes('2.js')).toBe(true);
+      expect(fileNames.includes('vendor.js')).toBe(true);
+      expect(fileNames.includes('file.js')).toBe(true);
+      // fs.writeFileSync('./what.js', js);
+      eval(js);
+      const scripts = Array.from(document.head.getElementsByTagName('script'));
+      expect(scripts.length).toBe(3);
+      const srcs = scripts.map(({ src }) => src);
+      expect(srcs.includes(CDN_PREFIX + '0.js')).toBe(true);
+      expect(srcs.includes('/public/1.js')).toBe(true);
+      expect(srcs.includes('/public/vendor.js')).toBe(true);
+      done();
+    });
+    compiler.outputFileSystem = new MemoryFileSystem();
+  });
 
-  // test('support upload multiple chunk and do not affect publich path in async chunk when we do not replace async chunk name', done => {
-  //   const compiler = webpack({
-  //     entry: {
-  //       file: path.join(__dirname, 'fixtures', 'file-a.js'),
-  //     },
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: '[name].js',
-  //       publicPath: '/public/',
-  //     },
-  //     plugins: [
-  //       new WebpackCdnUploadPlugin({
-  //         upload(content, name) {
-  //           return CDN_PREFIX + name;
-  //         },
-  //         replaceAsyncChunkName: false,
-  //       }),
-  //     ],
-  //   }, function(error, result) {
-  //     expect(error).toBeFalsy();
-  //     expect(result.compilation.errors.length).toBe(0);
-  //     const js = result.compilation.assets['file.js'].source();
-  //     const fileNames = Object.keys(result.compilation.assets);
-  //     expect(fileNames.includes('0.js')).toBe(true);
-  //     expect(fileNames.includes('1.js')).toBe(true);
-  //     expect(fileNames.includes('2.js')).toBe(true);
-  //     expect(fileNames.includes('3.js')).toBe(true);
-  //     expect(fileNames.includes('file.js')).toBe(true);
-  //     eval(js);
-  //     const scripts = Array.from(document.head.getElementsByTagName('script'));
-  //     expect(scripts.length).toBe(3);
-  //     const srcs = scripts.map(({ src }) => src);
-  //     expect(srcs.includes('/public/0.js')).toBe(true);
-  //     expect(srcs.includes('/public/1.js')).toBe(true);
-  //     expect(srcs.includes('/public/2.js')).toBe(true);
-  //     done();
-  //   });
-  //   compiler.outputFileSystem = new MemoryFileSystem();
-  // });
+  test('support upload multiple chunk and do not affect publich path in async chunk when we do not replace async chunk name', done => {
+    const compiler = webpack({
+      mode: 'development',
+      entry: {
+        file: path.join(__dirname, 'fixtures', 'file-a.js'),
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js',
+        publicPath: '/public/',
+      },
+      plugins: [
+        new WebpackCdnUploadPlugin({
+          upload(content, name) {
+            return CDN_PREFIX + name;
+          },
+          replaceAsyncChunkName: false,
+        }),
+      ],
+    }, function(error, result) {
+      expect(error).toBeFalsy();
+      expect(result.compilation.errors.length).toBe(0);
+      const js = result.compilation.assets['file.js'].source();
+      const fileNames = Object.keys(result.compilation.assets);
+      expect(fileNames.includes('0.js')).toBe(true);
+      expect(fileNames.includes('1.js')).toBe(true);
+      expect(fileNames.includes('2.js')).toBe(true);
+      expect(fileNames.includes('vendor.js')).toBe(true);
+      expect(fileNames.includes('file.js')).toBe(true);
+      eval(js);
+      const scripts = Array.from(document.head.getElementsByTagName('script'));
+      expect(scripts.length).toBe(3);
+      const srcs = scripts.map(({ src }) => src);
+      expect(srcs.includes('/public/0.js')).toBe(true);
+      expect(srcs.includes('/public/1.js')).toBe(true);
+      expect(srcs.includes('/public/vendor.js')).toBe(true);
+      done();
+    });
+    compiler.outputFileSystem = new MemoryFileSystem();
+  });
 
   // test('support replacement on single html-webpack-plugin', done => {
   //   const compiler = webpack({
